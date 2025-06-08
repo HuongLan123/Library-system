01e38ad3fccb666f1e74c7190f80575d3754a33d
Phạm Lan Hương <hustdream2k5@gmail.com>
2025-06-08 23:34:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from database import create_connection, reload_database_book, reload_database_loan, reload_database_reader
 def connect():
       # Tạo kết nối đến cơ sở dữ liệu SQLite
-    conn, cursor = create_connection("library.db")
+    conn, cursor = create_connection("library11.db")
     # Kiểm tra kết nối thành công hay không
     if not conn:
         print("Không thể kết nối đến cơ sở dữ liệu.")
@@ -21,7 +21,6 @@ def main():
         while True: 
             menu()  # Hiển thị menu chức năng
             ch = input("Nhập lựa chọn của bạn: ")
-
             if ch == "1":
                 print("Quản lý sách")
                 reload_database_book(conn, cursor)       # Nạp dữ liệu sách từ cơ sở dữ liệu vào bộ nhớ
@@ -41,14 +40,13 @@ def main():
 
             elif ch == "4":
                 print("Thoát chương trình")
-                break  # Thoát khỏi vòng lặp và kết thúc chương trình
-
+                break # Thoát khỏi vòng lặp và kết thúc chương trình
             else:
                 # Xử lý lựa chọn không hợp lệ
                 print("Lựa chọn không hợp lệ, vui lòng thử lại.")
-
     # Đóng kết nối cơ sở dữ liệu khi kết thúc chương trình
     conn.close()
+    return
 
 # Gọi hàm main nếu file được chạy trực tiếp
 if __name__ == "__main__":
